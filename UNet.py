7dfe3c8a860d1c5dd9e46631d59120e220d6df0b
limyunn <1569233527@qq.com>
2022-06-25 19:42:18 +0800

    model

--- UNet.py
+++ UNet.py
@@ -1,20 +1,24 @@
 import tensorflow as tf
 from tensorflow.keras.layers import *
+from tensorflow.keras.initializers import RandomNormal
 from tensorflow.keras import Model,layers,Sequential
 from tensorflow.keras.layers import Conv2D, Conv2DTranspose, Input, Cropping2D, add, Dropout, Reshape, Activation
-from tensorflow.keras.applications import vgg16
+from tensorflow.keras.applications import vgg16,resnet50
 import numpy as np
 from tensorflow.keras.utils import plot_model
 import tensorflow.keras.backend as K
 import math
 from keras_flops import get_flops
 import tensorflow_addons as tfa
+from dual_attention import *
 
 
-def Unet(input_shape=(224,224,3), num_classes=21):
+
+def Unet(input_shape=(224,224,3), num_classes=21,freeze=None):
     '''
       [B,H,W,C] = x.shape
     '''
+    #(None, 224, 224, 3)
     img_input=Input(shape=input_shape)
 
     assert img_input.shape[1] % 32 == 0,\
@@ -22,64 +26,66 @@ def Unet(input_shape=(224,224,3), num_classes=21):
     assert img_input.shape[2] % 32 == 0,\
         'The input_width should be divisible by 32'
 
-    model = vgg16.VGG16(
+    # -------------------------------------------------#
+    #              Encoder
+    # -------------------------------------------------#
+    vgg_model = vgg16.VGG16(
         include_top=False,
         weights='imagenet', input_tensor=img_input)
-    assert isinstance(model, Model), 'The model should be the instantiation of the keras Model '
 
+
+    assert isinstance(vgg_model, Model), 'The model should be the instantiation of the keras Model '
 
     # -------------------------------------------------#
     #              Decoder
     # -------------------------------------------------#
-    # 7,7,512 -> 14,14,1024 -> 14,14,512
-    P5 = UpSampling2D((2, 2))(model.output)
-    P5 = concatenate([model.get_layer(
-        name="block4_pool").output, P5], axis=-1)
-    P5 = Conv2D(512, (3, 3), padding="same")(P5)
-    P5 = BatchNormalization()(P5)
+    # 14,14,512 -> 28,28,512 -> 28,28,1024 -> 28,28,512
+    P5 = UpSampling2D((2, 2))(vgg_model.get_layer(name='block5_conv3').output)
+    att = danet(vgg_model.get_layer(name="block4_conv3").output)
+    P5 = concatenate([att, P5], axis=-1)
+    P5 = Conv2D(512, (3, 3), activation='relu', padding="same", kernel_initializer = RandomNormal(stddev=0.02))(P5)
+    P5 = Conv2D(512, (3, 3), activation='relu', padding="same", kernel_initializer = RandomNormal(stddev=0.02))(P5)
 
-    # 14,14,512 -> 28,28,768 -> 28,28,256
+
+    # 28,28,512 -> 56,56,512 -> 56,56,768 -> 56,56,256
     P4 = UpSampling2D((2, 2))(P5)
-    P4 = concatenate([model.get_layer(
-        name="block3_pool").output, P4], axis=-1)
-    P4 = Conv2D(256, (3, 3), padding="same")(P4)
-    P4 = BatchNormalization()(P4)
+    P4 = concatenate([vgg_model.get_layer(
+        name="block3_conv3").output, P4], axis=-1)
+    P4 = Conv2D(256, (3, 3), activation='relu', padding="same", kernel_initializer = RandomNormal(stddev=0.02))(P4)
+    P4 = Conv2D(256, (3, 3), activation='relu', padding="same", kernel_initializer = RandomNormal(stddev=0.02))(P4)
 
-    # 28,28,256 -> 56,56,384 -> 56,56,128
+
+    # 56,56,256 -> 112,112,256 -> 112,112,384 -> 112,112,128
     P3 = UpSampling2D((2, 2))(P4)
-    P3 = concatenate([model.get_layer(
-        name="block2_pool").output, P3], axis=-1)
-    P3 = Conv2D(128, (3, 3), padding="same")(P3)
-    P3 = BatchNormalization()(P3)
+    P3 = concatenate([vgg_model.get_layer(
+        name="block2_conv2").output, P3], axis=-1)
+    P3 = Conv2D(128, (3, 3), activation='relu', padding="same", kernel_initializer = RandomNormal(stddev=0.02))(P3)
+    P3 = Conv2D(128, (3, 3), activation='relu', padding="same", kernel_initializer = RandomNormal(stddev=0.02))(P3)
 
-    # 56,56,128 -> 112,112,192 -> 112,112,64
+
+    # 112,112,128 -> 224,224,128 -> 224,224,192 -> 224,224,64
     P2 = UpSampling2D((2, 2))(P3)
-    P2 = concatenate([model.get_layer(
-        name="block1_pool").output, P2], axis=-1)
-    P2 = Conv2D(64, (3, 3), padding="same")(P2)
-    P2 = BatchNormalization()(P2)
+    P2 = concatenate([vgg_model.get_layer(
+        name="block1_conv2").output, P2], axis=-1)
+    P2 = Conv2D(64, (3, 3), activation='relu', padding="same", kernel_initializer = RandomNormal(stddev=0.02))(P2)
+    P2 = Conv2D(64, (3, 3), activation='relu', padding="same", kernel_initializer = RandomNormal(stddev=0.02))(P2)
+    P1 = Conv2D(num_classes, (1, 1), padding="same")(P2)
+    P1 = Activation('softmax')(P1)
 
-    # UNet网络处理输入时进行了镜面放大2倍，所以最终的输入输出缩小了2倍
-    # 此处直接上采样原始大小
-    # 112,112,64 -> 224,224,64
-    x = UpSampling2D((2, 2))(P2)
-    x = Conv2D(64, (3, 3), padding="same")(x)
-    x = BatchNormalization()(x)
+    return Model(inputs=img_input, outputs=P1)
 
-    x = Conv2D(num_classes, (1, 1), padding="same")(x)
-    x = BatchNormalization()(x)
-    x = Activation('softmax')(x)
-
-    model = Model(inputs=img_input, outputs=x)
-    return model
 
 
 if __name__ == '__main__':
     model= Unet()
-    # print(m.get_weights()[2]) # 看看权重改变没，加载vgg权重测试用
     from tensorflow.keras.utils import plot_model
     plot_model(model, show_shapes=True, to_file='model_unet.png')
-    print(len(model.layers))
+
+    print(model.layers[18].gamma)#取出pam_module中可训练的gamma值
+
+    #<tf.Variable 'pam__module/pam_gamma:0' shape=(1,) dtype=float32, numpy=array([-0.06657953], dtype=float32)>
+    for _,layer in enumerate(model.layers):
+        print(_,layer.name)
     model.summary()
     # flops = get_flops(Unet(), batch_size=1) / (2 * 10 ** 9)
     # print(f"FLOPs:{flops:.1f}G")
--- dual_attention.py
+++ dual_attention.py
@@ -8,83 +8,210 @@ from tensorflow.keras import layers, Model
 from tensorflow.keras.utils import plot_model
 
 
-def CAM_Module(x):
+class ConvolutionBnActivation(tf.keras.layers.Layer):
     """
-       inputs :
-            x : input feature maps [B,H,W,C]
-       returns :
-            out : attention value + input feature
-              attention: B X (HxW) X (HxW)
     """
 
-    gamma = tf.Variable(tf.ones(1))
+    # def __init__(self, filters, kernel_size, strides=(1, 1), activation=tf.keras.activations.relu, **kwargs):
+    def __init__(self, filters, kernel_size, strides=(1, 1), padding="same", data_format=None, dilation_rate=(1, 1),
+                 groups=1, activation=None, kernel_initializer="glorot_uniform", bias_initializer="zeros",
+                 kernel_regularizer=None,
+                 bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None,
+                 use_batchnorm=False,
+                 axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, trainable=True,
+                 post_activation="relu", block_name=None):
+        super(ConvolutionBnActivation, self).__init__()
 
-    B,H,W,C= x.shape
+        # 2D Convolution Arguments
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.padding = padding
+        self.data_format = data_format
+        self.dilation_rate = dilation_rate
+        self.activation = activation
+        self.use_bias = not (use_batchnorm)
+        self.kernel_initializer = kernel_initializer
+        self.bias_initializer = bias_initializer
+        self.kernel_regularizer = kernel_regularizer
+        self.bias_regularizer = bias_regularizer
+        self.activity_regularizer = activity_regularizer
+        self.kernel_constraint = kernel_constraint
+        self.bias_constraint = bias_constraint
 
-    assert K.image_data_format() == "channels_last"
+        # Batch Normalization Arguments
+        self.axis = axis
+        self.momentum = momentum
+        self.epsilon = epsilon
+        self.center = center
+        self.scale = scale
+        self.trainable = trainable
 
-    query = tf.keras.layers.Reshape((-1, C))(x)
-    key = tf.keras.layers.Reshape((-1, C))(x)
+        self.block_name = block_name
 
-    energy = tf.linalg.matmul(query, key, transpose_a=True)
+        self.conv = None
+        self.bn = None
+        # tf.keras.layers.BatchNormalization(scale=False, momentum=0.9)
+        self.post_activation = tf.keras.layers.Activation(post_activation)
 
-    attention = Activation("softmax")(energy)
+    def build(self, input_shape):
+        self.conv = tf.keras.layers.Conv2D(
+            filters=self.filters,
+            kernel_size=self.kernel_size,
+            strides=self.strides,
+            padding=self.padding,
+            data_format=self.data_format,
+            dilation_rate=self.dilation_rate,
+            activation=self.activation,
+            use_bias=self.use_bias,
+            kernel_initializer=self.kernel_initializer,
+            bias_initializer=self.bias_initializer,
+            kernel_regularizer=self.kernel_regularizer,
+            bias_regularizer=self.bias_regularizer,
+            activity_regularizer=self.activity_regularizer,
+            kernel_constraint=self.kernel_constraint,
+            bias_constraint=self.bias_constraint,
+            name=self.block_name + "_conv" if self.block_name is not None else None
 
-    value = tf.keras.layers.Reshape((-1, C))(x)
-    out = tf.linalg.matmul(attention, value, transpose_b=True)
+        )
 
-    out = tf.keras.layers.Reshape(x.shape[1:])(out)
+        self.bn = tf.keras.layers.BatchNormalization(
+            axis=self.axis,
+            momentum=self.momentum,
+            epsilon=self.epsilon,
+            center=self.center,
+            scale=self.scale,
+            trainable=self.trainable,
+            name=self.block_name + "_bn" if self.block_name is not None else None
+        )
 
-    out = layers.add([gamma * out , x])
+    def call(self, x, training=None):
+        x = self.conv(x)
+        x = self.bn(x, training=training)
+        x = self.post_activation(x)
 
-    return out
+        return x
 
-def PAM_Module(x):
-    """
-       inputs :
-            x : input feature maps( B X C X H X W)
-       returns :
-            out : attention value + input feature
-            attention: B X (HxW) X (HxW)
-    """
+    def compute_output_shape(self, input_shape):
+        print(input_shape)
+        return [input_shape[0], input_shape[1], input_shape[2], self.filters]
 
-    gamma = tf.Variable(tf.ones(1))
 
-    B, H, W, C = x.shape
+class PAM_Module(tf.keras.layers.Layer):
+    def __init__(self, filters):
+        super(PAM_Module, self).__init__()
 
-    query = Conv2D(filters=C//8,kernel_size=1,padding='same',activation='relu')(x)
-    key = Conv2D(filters=C//8,kernel_size=1,padding='same',activation='relu')(x)
-    value = Conv2D(filters=C,kernel_size=1,padding='same',activation='relu')(x)
+        self.filters = filters
 
-    assert K.image_data_format() == "channels_last"
+        axis = 3 if K.image_data_format() == "channels_last" else 1
+        self.concat = tf.keras.layers.Concatenate(axis=axis)
 
-    query = tf.keras.layers.Reshape((H * W, -1))(query)
-    key = tf.keras.layers.Reshape((H * W, -1))(key)
+        self.conv1x1_bn_relu_1 = ConvolutionBnActivation(filters//8, (1, 1))
+        self.conv1x1_bn_relu_2 = ConvolutionBnActivation(filters//8, (1, 1))
+        self.conv1x1_bn_relu_3 = ConvolutionBnActivation(filters, (1, 1))
 
-    energy = tf.linalg.matmul(query, key, transpose_b=True)
+        self.gamma = None
 
-    attention = Activation("softmax")(energy)
+        self.softmax = tf.keras.layers.Activation("softmax")
 
-    value = tf.keras.layers.Reshape((H * W, -1))(value)
-    out = tf.linalg.matmul(attention,value)
+    def build(self, input_shape):
+        self.gamma = self.add_weight(
+            shape=(1,),
+            initializer="random_normal",
+            name="pam_gamma",
+            trainable=True,
+        )
 
-    out = tf.keras.layers.Reshape(x.shape[1:])(out)
-    out = layers.Add()([gamma * out , x])
+    def call(self, x, training=None):
+        B, H, W, C = x.shape
 
-    return out
+        query = self.conv1x1_bn_relu_1(x, training=training)
+        key = self.conv1x1_bn_relu_2(x, training=training)
+
+        # [B, H * W, C//8]
+        query = tf.keras.layers.Reshape((H * W, -1))(query)
+        key = tf.keras.layers.Reshape((H * W, -1))(key)
+
+        # [B, H * W, C//8] -> [B, H * W, H * W]
+        energy = tf.linalg.matmul(query, key, transpose_b=True)
+        attention = self.softmax(energy)
+
+        # [B, H , W, C]
+        value = self.conv1x1_bn_relu_3(x, training=training)
+
+        # [B, H * W, C] -> [B, H * W, C]
+        value = tf.keras.layers.Reshape((H * W, -1))(value)
+        out = tf.linalg.matmul(attention, value)
+
+        # [B, H * W, C] -> [B, H, W, C]
+        out = tf.keras.layers.Reshape(x.shape[1:])(out)
+
+        out = self.gamma * out + x
+
+        return out
+
+
+class CAM_Module(tf.keras.layers.Layer):
+    def __init__(self, filters):
+        super(CAM_Module, self).__init__()
+
+        self.filters = filters
+
+        self.gamma = None
+
+        self.softmax = tf.keras.layers.Activation("softmax")
+
+    def build(self, input_shape):
+        self.gamma = self.add_weight(
+            shape=(1,),
+            initializer="random_normal",
+            name="cam_gamma",
+            trainable=True,
+        )
+
+    def call(self, x, training=None):
+
+
+        B, H, W, C = x.shape
+
+        # [B, H * W, C]
+        query = tf.keras.layers.Reshape((-1, C))(x)
+
+        # [B, H * W, C]
+        key = tf.keras.layers.Reshape((-1, C))(x)
+
+        # [B, C , C]
+        energy = tf.linalg.matmul(query, key, transpose_a=True)
+
+        attention = self.softmax(energy)
+
+        # [B, H , W, C] -> [B, H * W, C]
+        value = tf.keras.layers.Reshape((-1, C))(x)
+
+        # [B, H * W, C] -> [B, C, H * W]
+        out = tf.linalg.matmul(attention, value, transpose_b=True)
+
+        # [B, C, H * W] -> [B, H * W, C]
+        out = tf.keras.layers.Permute((2,1))(out)
+
+        # [B, H * W, C] -> [B, H, W, C]
+        out = tf.keras.layers.Reshape(x.shape[1:])(out)
+
+        out = self.gamma * out + x
+
+        return out
 
 
 def danet(inputs):
-    x1 = CAM_Module(inputs)
-    x2 = PAM_Module(inputs)
+    b, h, w, c = inputs.shape
+    x1 = CAM_Module(c)(inputs)
+    x2 = PAM_Module(c)(inputs)
     x = layers.add([x1, x2])
     return x
 
-if __name__ == '__main__':
-    inputs = keras.Input(shape=[26, 26, 512])
-    outputs = danet(inputs)
-    model = Model(inputs, outputs)
-    model.summary()
-    plot_model(model, show_shapes=True, to_file='model_unet.png')
-
+# inputs = Input(shape=[26, 26, 512])
+# outputs = danet(inputs)
+# model = Model(inputs, outputs)
+# model.summary()
+# plot_model(model, show_shapes=True, to_file='model_unet.png')
 
